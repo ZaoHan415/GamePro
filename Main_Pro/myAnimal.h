8d0ea828cd2426d45b587bc8722023edd22f4f06
Song-XY <Song-XY@users.noreply.github.com>
2018-12-12 09:52:16 +0800

    animal

--- Main_Pro/myAnimal.h
+++ Main_Pro/myAnimal.h
@@ -1,11 +1,28 @@
 #ifndef MYANIMAL_H
 #define MYANIMAL_H
 #include <QGraphicsItem>
+#include "Hexagon.h"
 
-class myAnimal : public QGraphicsItem
+class myAnimal : public QObject
 {
+    Q_OBJECT
 public:
     myAnimal();
+    QPoint get_position()
+    {
+        return position;
+    }
+    int get_direction()
+    {
+        return direction;
+    }
+private:
+    QPoint position;
+    int direction;//0 to 5
+    qreal time_per_step=1000;
+    QTimer* m_timer;
+    Hexagon* m_hex;
+public slots:
 };
 
 #endif // MYANIMAL_H
