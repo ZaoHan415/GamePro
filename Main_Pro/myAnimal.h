1d98f19a2732594cacdf9312487288f5377eb1b3
ZaoHan415 <36807528+ZaoHan415@users.noreply.github.com>
2018-12-12 09:31:39 +0800

    删掉了写的混乱的部分
    animal部分交给song-XY

--- Main_Pro/myAnimal.h
+++ Main_Pro/myAnimal.h
@@ -6,31 +6,6 @@ class myAnimal : public QObject
 {
     Q_OBJECT
 public:
-    myAnimal(QPoint);
-    //virtual void advance(int phase) override;
-    QPoint PosT()
-    {
-        return position;
-    }
-    int Direction()
-    {
-        return direction;
-    }
-private:
-    QPoint position;
-    //move one unit after [time_step] mseconds
-    qreal time_step = 1000;
-    int direction;//from 1 to 6
-    QTimer* m_timer;
-    Hexagon* m_hex;
-public slots:
-    //intially, -1 means counter clock-wise
-    //+1 means clock-wise
-    void changeDirection(int x);
-    void moveToNext();
-    //void outOfBorder();
-signals:
-    void directionChanged(int x);
 };
 
 #endif // MYANIMAL_H
